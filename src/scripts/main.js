2fe88b7736a1b95538e90b18624259615895c4b0
Ewerton Luiz <ewerton.luiz_1994@hotmail.com>
2024-12-22 11:51:21 -0300

    inicial

---
+++ src/scripts/main.js
@@ -0,0 +1,33 @@
+AOS.init();
+
+const dataDoEvento = new Date("jul 05, 2025 19:00:00");
+const timeStampDoEvento = dataDoEvento.getTime();
+
+const contaAsHoras = setInterval (function() {
+    const agora = new Date();
+    const timeStampAtual = agora.getTime();
+
+    const distanciaAteOEvento = timeStampDoEvento - timeStampAtual;
+
+    const diaEmMs = 1000 * 60 * 60 * 24;
+    const horasEmMs = 1000 * 60 * 60;
+    const minutosEmMs = 1000 * 60;
+    const segundosEmMs = 1000;
+
+    const diasAteOEvento = Math.floor(distanciaAteOEvento / diaEmMs);
+    const horasAteOEvento = Math.floor((distanciaAteOEvento % diaEmMs) / horasEmMs);
+    const minutosAteOEvento = Math.floor((distanciaAteOEvento % horasEmMs) / minutosEmMs);
+    const segundosAteOEvento = Math.floor((distanciaAteOEvento % minutosEmMs) / segundosEmMs);
+
+    console.log(diasAteOEvento);
+    console.log(horasAteOEvento);
+    console.log(minutosAteOEvento);
+    console.log(segundosAteOEvento);
+
+    document.getElementById('contador').innerHTML = `${diasAteOEvento}d ${horasAteOEvento}h ${minutosAteOEvento}m ${segundosAteOEvento}s`;
+
+    if (distanciaAteOEvento < 0) {
+        clearInterval(contaAsHoras);
+        document.getElementById('contador').innerHTML = `Acabou a festa`
+    }
+}, 1000)
